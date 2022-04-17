a823cc7d4d865c478d545bc749740b5938d49c46
waigoma <waichi0302@gmail.com>
2022-04-18 02:39:37 +0900

    update logger

--- src/main/kotlin/data/YamlLoader.kt
+++ src/main/kotlin/data/YamlLoader.kt
@@ -6,19 +6,26 @@ import java.io.InputStream
 
 class YamlLoader {
     companion object {
+        private val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
+
         fun load(path: String) {
+            logger.info("Loading yaml file: $path...")
             val inputStream: InputStream = File(path).inputStream()
 
             val yaml = Yaml()
             val data = yaml.load<Map<String, Any>>(inputStream)
+            logger.info("Yaml file loaded: $path!")
 
             val source = data["source"]!!.toString()
 
             if (source == "ENV") {
+                logger.info("Loading ENV variables...")
                 DataManager.initializeEnv()
+                logger.info("ENV variables loaded!")
                 return
             }
 
+            logger.info("Loading data from yml...")
             val botToken = data["bot-token"]!!.toString()
 
             val databaseUrl = data["database-url"]!!.toString()
@@ -27,6 +34,7 @@ class YamlLoader {
             val databasePassword = data["database-password"]!!.toString()
 
             DataManager.initializeData(botToken, databaseUrl, databaseName, databaseUser, databasePassword)
+            logger.info("Data loaded!")
         }
     }
 }
