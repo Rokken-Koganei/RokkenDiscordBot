a2dfb949bd73248c02819f5070f9738dc866e5d9
waigoma <waichi0302@gmail.com>
2022-04-20 00:51:50 +0900

    summarize method

--- src/main/kotlin/discord/DiscordJoin.kt
+++ src/main/kotlin/discord/DiscordJoin.kt
@@ -1,12 +1,9 @@
 package discord
 
-import discord.reaction.FirstReactionListener
-import net.dv8tion.jda.api.EmbedBuilder
+import discord.message.FirstMessage
 import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import java.awt.Color
 
 class DiscordJoin : ListenerAdapter() {
     companion object {
@@ -17,27 +14,6 @@ class DiscordJoin : ListenerAdapter() {
         val user = event.user
         joinedGuild = event.guild
 
-        user.openPrivateChannel().queue { channel ->
-            channel.sendMessage("**__ロッ研オンライン部室へようこそ！__**\nこの部室は、交流の場を作りたいという願いのもと、作られました！\nこれから、いくつか質問に答えてくださると助かります！").queue()
-            channel.sendMessageEmbeds(createEmbed()).queue {
-                it.addReaction("⭕").queue()
-                it.addReaction("❌").queue()
-                it.jda.addEventListener(FirstReactionListener())
-            }
-        }
-    }
-
-    private fun createEmbed(): MessageEmbed {
-        val embed = EmbedBuilder()
-
-        embed.setColor(Color.GREEN)
-
-        embed.setTitle("現在部員ですか？")
-        embed.setDescription("リアクションをクリックまたはタップして、質問に答えてください！")
-
-        embed.addField("部員または入部予定", ":o: を選択", false)
-        embed.addField("部員ではないが興味がある", ":x: を選択", false)
-
-        return embed.build()
+        FirstMessage().firstMessage(user)
     }
 }
--- src/main/kotlin/discord/message/FirstMessage.kt
+++ src/main/kotlin/discord/message/FirstMessage.kt
@@ -1,4 +1,34 @@
 package discord.message
 
+import discord.reaction.FirstReactionListener
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.entities.User
+import java.awt.Color
+
 class FirstMessage {
+    fun firstMessage(user: User) {
+        user.openPrivateChannel().queue { channel ->
+            channel.sendMessage("**__ロッ研オンライン部室へようこそ！__**\nこの部室は、交流の場を作りたいという願いのもと、作られました！\nこれから、いくつか質問に答えてくださると助かります！").queue()
+            channel.sendMessageEmbeds(createEmbed()).queue {
+                it.addReaction("⭕").queue()
+                it.addReaction("❌").queue()
+                it.jda.addEventListener(FirstReactionListener())
+            }
+        }
+    }
+
+    private fun createEmbed(): MessageEmbed {
+        val embed = EmbedBuilder()
+
+        embed.setColor(Color.GREEN)
+
+        embed.setTitle("現在部員ですか？")
+        embed.setDescription("リアクションをクリックまたはタップして、質問に答えてください！")
+
+        embed.addField("部員または入部予定", ":o: を選択", false)
+        embed.addField("部員ではないが興味がある", ":x: を選択", false)
+
+        return embed.build()
+    }
 }
