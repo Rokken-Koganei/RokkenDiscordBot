e0548163a13437e9dd1c8140a33ec1afd8dfa1a2
waigoma <waichi0302@gmail.com>
2022-04-20 00:35:30 +0900

    add Event, cache policy

--- src/main/kotlin/discord/DiscordMain.kt
+++ src/main/kotlin/discord/DiscordMain.kt
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.MemberCachePolicy
 import javax.security.auth.login.LoginException
 
 class DiscordMain {
@@ -22,16 +23,14 @@ class DiscordMain {
                 )
                 .addEventListeners(
                     DiscordCommand(),
+                    DiscordAdminCommand(),
                     DiscordJoin()
                 )
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .setActivity(Activity.playing("作業"))
                 .build()
         } catch (e: LoginException) {
             e.printStackTrace()
         }
-    }
-
-    fun notify(message: String) {
-
     }
 }
