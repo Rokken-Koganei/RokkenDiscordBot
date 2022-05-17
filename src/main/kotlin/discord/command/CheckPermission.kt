e1394af72590c924c8dfcdcea57c83c9225f9353
waigoma <waichi0302@gmail.com>
2022-05-17 12:28:08 +0900

    add check command permission method

---
+++ src/main/kotlin/discord/command/CheckPermission.kt
@@ -0,0 +1,18 @@
+package discord.command
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+class CheckPermission {
+    companion object {
+        fun hasCommandPermission(event: SlashCommandInteractionEvent, roleStr: String): Boolean {
+            // 鯖からのメッセージ以外は受け付けない
+            if (!event.isFromGuild) return false
+
+            val guild = event.guild
+            val role = guild!!.getRoleById(roleStr)!!
+
+            // ロールが幹部でなければ無視
+            return event.member!!.roles.contains(role)
+        }
+    }
+}
