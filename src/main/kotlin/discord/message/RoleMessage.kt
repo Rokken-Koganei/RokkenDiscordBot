8b73e4ca778e1aab928fdc0151a4cb604d2599df
waigoma <waichi0302@gmail.com>
2022-05-19 18:09:19 +0900

    add RoleMessage.kt

---
+++ src/main/kotlin/discord/message/RoleMessage.kt
@@ -0,0 +1,40 @@
+package discord.message
+
+import discord.reaction.RoleSelectReactionListener
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
+import java.awt.Color
+
+class RoleMessage {
+    fun send(event: MessageReactionAddEvent) {
+        event.channel.sendMessageEmbeds(createEmbed()).queue {
+            it.addReaction("🎤").queue()
+            it.addReaction("🎸").queue()
+            it.addReaction("🥁").queue()
+            it.addReaction("🪕").queue()
+            it.addReaction("🎹").queue()
+            it.addReaction("❌").queue()
+            it.jda.addEventListener(RoleSelectReactionListener())
+            it.jda.removeEventListener(this)
+        }
+    }
+
+    private fun createEmbed(): MessageEmbed {
+        val embed = EmbedBuilder()
+
+        embed.setColor(Color.GREEN)
+
+        embed.setTitle("希望楽器は何ですか？")
+        embed.setDescription("リアクションをクリックまたはタップして、質問に答えてください！\n選択が終了したら、:x: を選択してください。")
+
+        embed.addField("Vo.", ":microphone: を選択", true)
+        embed.addField("Gt.", ":guitar: を選択", true)
+        embed.addField("Dr.", ":drum: を選択", true)
+        embed.addField("Ba.", ":banjo: を選択", true)
+        embed.addField("Key.", ":musical_keyboard: を選択", true)
+        embed.addField("終了", ":x: を選択", true)
+
+        return embed.build()
+    }
+}
