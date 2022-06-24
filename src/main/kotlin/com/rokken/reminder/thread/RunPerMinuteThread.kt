7be005e730d943bff7ac4b1eb1a6c41e5c91b56f
waigoma <waichi0302@gmail.com>
2022-06-24 10:01:44 +0900

    update RunPerMinuteThread

--- src/main/kotlin/com/rokken/reminder/thread/RunPerMinuteThread.kt
+++ src/main/kotlin/com/rokken/reminder/thread/RunPerMinuteThread.kt
@@ -3,23 +3,24 @@ package com.rokken.reminder.thread
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-class RunPerMinuteThread(private val runnable: Runnable) : Thread() {
+class RunPerMinuteThread(private val offset: String) : Thread() {
     override fun run() {
         while (true) {
             val localDateTime = LocalDateTime.now()
-            val now = localDateTime.toInstant(ZoneOffset.of("Asia/Tokyo")).epochSecond
-            localDateTime.withSecond(0)
-            localDateTime.plusMinutes(1)
-            val minuteLater = localDateTime.toInstant(ZoneOffset.of("Asia/Tokyo")).epochSecond
+            val now = localDateTime.toInstant(ZoneOffset.of(offset)).epochSecond
 
-            val sleepTime = minuteLater - now
+            val nextTime = LocalDateTime.now()
+                .withSecond(0)
+                .plusMinutes(1)
+                .toInstant(ZoneOffset.of(offset)).epochSecond
+
+            val sleepTime = nextTime - now
 
             try {
-                sleep(sleepTime)
+                sleep(sleepTime * 1000)
             } catch (e: InterruptedException) {
                 e.printStackTrace()
             }
-            runnable.run()
         }
     }
 }
