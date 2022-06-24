7849985cbb35719dc10d7d04c3c0c68c4c9fe329
waigoma <waichi0302@gmail.com>
2022-06-24 10:01:54 +0900

    update ReminderMain.kt

--- src/main/kotlin/com/rokken/reminder/ReminderMain.kt
+++ src/main/kotlin/com/rokken/reminder/ReminderMain.kt
@@ -1,7 +1,16 @@
 package com.rokken.reminder
 
-class ReminderMain {
-    fun start() {
+import com.rokken.reminder.thread.RunPerDayThread
+import com.rokken.reminder.thread.RunPerMinuteThread
 
+class ReminderMain {
+    private val offset = "+9"
+
+    fun start() {
+        val runPerMinuteThread = RunPerMinuteThread(offset)
+        val runPerDayThread = RunPerDayThread(offset)
+
+        runPerMinuteThread.start()
+        runPerDayThread.start()
     }
 }
