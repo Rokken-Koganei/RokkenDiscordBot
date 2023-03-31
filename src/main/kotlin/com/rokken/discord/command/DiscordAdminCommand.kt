cad5a5582ce7cb9b2c2c5f11fd6d04f8973951ce
waigoma <waichi0302@gmail.com>
2023-03-31 20:53:42 +0900

    implement role migration command

--- src/main/kotlin/com/rokken/discord/command/DiscordAdminCommand.kt
+++ src/main/kotlin/com/rokken/discord/command/DiscordAdminCommand.kt
@@ -1,7 +1,8 @@
 package com.rokken.discord.command
 
-import com.rokken.discord.RoleManager
+import com.rokken.discord.role.RoleManager
 import com.rokken.discord.message.FirstMessage
+import com.rokken.discord.role.GradeRole
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
@@ -20,6 +21,7 @@ class DiscordAdminCommand: ListenerAdapter() {
         const val SUBCOMMAND_FIRST = "first"
         const val SUBCOMMAND_HELP = "help"
         const val SUBCOMMAND_BROADCAST_HELP = "bhelp"
+        const val SUBCOMMAND_MIGRATION = "migration"
 
         private val userOption = OptionData(OptionType.USER, "user", "ユーザー名").setRequired(true)
 
@@ -31,7 +33,9 @@ class DiscordAdminCommand: ListenerAdapter() {
             SubcommandData(SUBCOMMAND_FIRST, "初期ロール付与用メッセージを送信する。")
                 .addOptions(userOption),
             SubcommandData(SUBCOMMAND_HELP, "幹部コマンドのヘルプを表示する。"),
-            SubcommandData(SUBCOMMAND_BROADCAST_HELP, "幹部コマンドのヘルプを全ての人が見えるように表示する。")
+            SubcommandData(SUBCOMMAND_BROADCAST_HELP, "幹部コマンドのヘルプを全ての人が見えるように表示する。"),
+            SubcommandData(SUBCOMMAND_MIGRATION, "年度が変わるときに必ず実行すること。ロール等の更新を行う。")
+                .addOptions(OptionData(OptionType.STRING, "最終確認", "本当にいい？").setRequired(false))
         )
     }
 
@@ -73,6 +77,27 @@ class DiscordAdminCommand: ListenerAdapter() {
             SUBCOMMAND_HELP -> event.replyEmbeds(helpEmbed()).setEphemeral(true).queue()
 
             SUBCOMMAND_BROADCAST_HELP -> event.replyEmbeds(helpEmbed()).setEphemeral(false).queue()
+
+            SUBCOMMAND_MIGRATION -> {
+                val gr = GradeRole()
+                val options = event.options
+                val msg = "/admin migration confirm で世代交代プログラムが実行されます。"
+
+                when (options.size) {
+                    0 -> {
+                        val rtMsg = gr.migrate(false)
+                        event.reply("「卒業する期 = ${rtMsg.first}, 入学する期 = ${rtMsg.second}」この値が正常であれば、\n$msg").setEphemeral(false).queue()
+                    }
+                    1 -> {
+                        if (options[0].asString == "confirm") {
+                            gr.migrate(true)
+                            event.reply("実行します...").setEphemeral(false).queue()
+                        } else
+                            event.reply(msg).setEphemeral(false).queue()
+                    }
+                    else -> event.reply("引数多すぎ問題発生中。\n $msg").setEphemeral(false).queue()
+                }
+            }
 
             else -> event.reply("不明なコマンドです。\n/admin help でヘルプを表示します。").setEphemeral(true).queue()
         }
--- src/main/kotlin/com/rokken/discord/role/GradeRole.kt
+++ src/main/kotlin/com/rokken/discord/role/GradeRole.kt
@@ -1,7 +1,55 @@
 package com.rokken.discord.role
 
-import java.time.LocalDate
+import com.rokken.discord.DiscordMain
+import net.dv8tion.jda.api.entities.Role
 
 class GradeRole {
+    private val cg = CalcGrade()
+    private val guild = DiscordMain.rokkenGuild
+    private val terms = ArrayList<Role?>(4)
+    private val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
 
+    init {
+        terms.add(guild.getRoleById(RoleManager.GRADE_1))
+        terms.add(guild.getRoleById(RoleManager.GRADE_2))
+        terms.add(guild.getRoleById(RoleManager.GRADE_3))
+        terms.add(guild.getRoleById(RoleManager.GRADE_4))
+    }
+
+    /**
+     * 毎年年度が替わるごとに実行。
+     * 期を更新するために使う。
+     */
+    fun migrate(isRun: Boolean): Pair<Int, String> {
+        // 卒業する人の期を求める
+        val gradGrade = cg.calcGrade(5)
+        // 変更対象のロールを検索
+        val termRole = findRole(gradGrade.toString())
+
+        // ロール名変更処理
+        if (isRun) {
+            termRole.manager.setName("${gradGrade + 4}期")
+            logger.info("ロールのマイグレーションが完了しました。")
+        }
+
+        return Pair(gradGrade,termRole.name)
+    }
+
+    /**
+     * 選択した学年にふさわしいロールを取得する
+     * @param selectedGrade 選択された学年
+     * @return ?期のロール
+     */
+    fun getTermRole(selectedGrade: Int) : Role {
+        return findRole(cg.calcGrade(selectedGrade).toString())
+    }
+
+    private fun findRole(termStr: String) : Role {
+        for (term in terms) {
+            // 名前に何期 (termStr) が含まれていたら返す
+            if (term!!.name.contains(termStr))
+                return term
+        }
+        return terms[0]!!
+    }
 }
